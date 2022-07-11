40c89397cd77a42ce0119f74ef3662febc8cac78
fengbujue <1125238900@qq.com>
2022-07-11 15:54:50 +0800

    引入了vant组件库

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import '@/assets/iconfont/iconfont.css'
 import '@/styles/index.less'
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
+Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
